f40de84e529ffd420c88c1c79a1dadc19ab15fcf
Perazzojoao <perazzojoao@gmail.com>
2024-10-27 23:41:04 -0300

    adding logger and http exception filter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
-import { Module } from '@nestjs/common';
+import { Module, ConsoleLogger } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
+import { APP_INTERCEPTOR, APP_FILTER } from '@nestjs/core';
+import { HttpExceptionFilter } from './resources/filters/http-exception.filter';
+import { LoggerInterceptor } from './resources/interceptors/logger.interceptor';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggerInterceptor,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+    ConsoleLogger,
+  ],
 })
 export class AppModule {}
