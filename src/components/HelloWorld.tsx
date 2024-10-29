80ab0d386f987b1890c58fc207372aace423b728
Perazzojoao <perazzojoao@gmail.com>
2024-10-28 21:29:00 -0300

    first commit

---
+++ src/components/HelloWorld.tsx
@@ -0,0 +1,11 @@
+'use client';
+import { useState } from "react";
+
+const HelloWorld = () => {
+  const [test, setTest] = useState();
+  return ( 
+    <h1>Hello World</h1>
+   );
+}
+ 
+export default HelloWorld;
