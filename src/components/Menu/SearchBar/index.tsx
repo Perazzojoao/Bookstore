79d929b51da7e68d998c33c9c45475abfa96dae5
Perazzojoao <perazzojoao@gmail.com>
2024-11-05 15:07:45 -0300

    Correção de erro de build

--- src/components/Menu/SearchBar/index.tsx
+++ src/components/Menu/SearchBar/index.tsx
@@ -1,9 +1,14 @@
 'use client';
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const SearchBar = () => {
   const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    console.log(search);
+  }, []);
+
   return ( 
     <div className="w-full px-4 sm:px-0">
       <input type="text" placeholder="Estou procurando por..." className="w-full px-4 py-2 rounded-lg text-zinc-800 placeholder:text-zinc-800" onChange={(e) => setSearch(e.target.value)}/>
