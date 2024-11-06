a5c6b907068f66110e451a5fda51e02cf841c669
Perazzojoao <perazzojoao@gmail.com>
2024-11-06 15:33:37 -0300

    Ajuste de componente

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -4,13 +4,9 @@ import { SearchBookContext } from "@/context/search_books.contex";
 import { useRouter } from "next/navigation";
 import { FormEvent, useContext, useState } from "react";
 
-interface SearchBarProps {
-  defaultSearch?: string;
-}
-
-const SearchBar = ({ defaultSearch = '' }: SearchBarProps) => {
-  const [search, setSearch] = useState(defaultSearch);
-  const { setSearchTitle } = useContext(SearchBookContext);
+const SearchBar = () => {
+  const [search, setSearch] = useState('');
+  const { searchTitle, setSearchTitle } = useContext(SearchBookContext);
   const router = useRouter();
 
   function handleSubmit(e: FormEvent<HTMLFormElement>) {
@@ -22,7 +18,7 @@ const SearchBar = ({ defaultSearch = '' }: SearchBarProps) => {
   return (
     <div className="w-full px-4 sm:px-0">
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Estou procurando por..." defaultValue={search} className="w-full px-4 py-2 rounded-lg text-zinc-800 placeholder:text-zinc-800" onChange={(e) => setSearch(e.target.value)} />
+        <input type="text" placeholder="Estou procurando por..." defaultValue={searchTitle} className="w-full px-4 py-2 rounded-lg text-zinc-800 placeholder:text-zinc-800" onChange={(e) => setSearch(e.target.value)} />
       </form>
     </div>
   );
